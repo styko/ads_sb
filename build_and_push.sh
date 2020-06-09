1c61b992cea6870061a4236a794602eeae2c5300
Michal Styk <michal.styk@powerex.io>
2020-06-09 21:03:50 +0200

    added tokens

--- build_and_push.sh
+++ build_and_push.sh
@@ -10,12 +10,7 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 mvn clean compile package -f "$DIR/pom.xml"
-heroku container:push worker -a quiet-scrubland-05864
-heroku container:release worker -a quiet-scrubland-05864
 
-
-#Picked up JAVA_TOOL_OPTIONS: -XX:+UseContainerSupport -Xmx300m -Xss512k -XX:CICompilerCount=2 -Dfile.encoding=UTF-8
-#: Starting process with command `java git -jar target/realestate_email_aggregator-1.0-SNAPSHOT.jar`
-#2020-06-08T11:12:22.956247+00:00 app[web.1]: Create a Procfile to customize the command used to run this process: https://devcenter.heroku.com/articles/procfile
-#2020-06-08T11:12:23.080227+00:00 app[web.1]: Setting JAVA_TOOL_OPTIONS defaults based on dyno size. Custom settings will override them.
-#2020-06-08T11:12:23.086547+00:00 app[web.1]: Picked up JAVA_TOOL_OPTIONS: -XX:+UseContainerSupport -Xmx300m -Xss512k -XX:CICompilerCount=2 -Dfile.encoding=UTF-8
+docker build -t ads-sb "$DIR/."
+docker tag ads-sb:latest dokku/ads-sb:latest:v1
+docker push dokku/ads-sb:latest:v1
